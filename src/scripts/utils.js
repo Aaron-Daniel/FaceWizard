6eec342a4122a7573cd44d0a46b5b914f3075837
Aaron Daniel <aaron@Aarons-MacBook-Pro.local>
2019-07-17 19:36:49 -0700

    Playing with OAuth

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -22,7 +22,7 @@
 const API_ENDPOINT = "https://api.veritone.com/v3/graphql";
 const AUTH_BASE  = "https://api.veritone.com/v1/admin/oauth/authorize?scope=all&response_type=token&client_id=";
 const CLIENT_ID = "caf06532-6787-45f3-a2e4-8ff02a55012f";
-const HOME_URL = "https://veritone-sample-app.netlify.com";
+const HOME_URL = "https://musing-boyd-05e7b5.netlify.com/";
 const TEXT_VALIDATION_ERROR_MSG = "That doesn't look right. Try again.";
 const DAYS_TO_STORE_TOKEN = 1;
 let TDO_ID = null;
@@ -89,9 +89,9 @@ function getCookie(cname) {
 
 function getOAuthLink() {
 	
-    let clientID  = CLIENT_ID;
-    let auth_base = AUTH_BASE;
-    let redirect = "&redirect_uri=" + HOME_URL;
+    let clientID  = CLIENT_ID; //"caf06532-6787-45f3-a2e4-8ff02a55012f"
+    let auth_base = AUTH_BASE; //"https://api.veritone.com/v1/admin/oauth/authorize?scope=all&response_type=token&client_id="
+    let redirect = "&redirect_uri=" + HOME_URL; //https://musing-boyd-05e7b5.netlify.com/
     var OAuthLink = auth_base + clientID + redirect;
 	
     return OAuthLink;
