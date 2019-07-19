27c860c019614c7d9af30cf70ebf3094d0d7c13c
Aaron Daniel <aaron@Aarons-MacBook-Pro.local>
2019-07-18 20:51:12 -0700

    trying to format query correctly

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -30,6 +30,7 @@ let TDO_ID = 580940766;
 let TDO_JSON = null;
 let _token = null;
 let faceImageAddress = "https://www.uni-regensburg.de/Fakultaeten/phil_Fak_II/Psychologie/Psy_II/beautycheck/english/prototypen/w_sexy_gr.jpg"
+let test = 123;
 let ApiCallForEngineList = `query {
   engineCategories {
     records {
